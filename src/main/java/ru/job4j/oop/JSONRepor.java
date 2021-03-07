75e5aee971983f6bc09da19d31a69ccdf28a4a05
Albina <albafox@yandex.ru>
2021-03-07 19:40:00 +0300

    5. Аннотация @Override [#472863]

--- src/main/java/ru/job4j/oop/JSONRepor.java
+++ src/main/java/ru/job4j/oop/JSONRepor.java
@@ -1,6 +1,8 @@
 package ru.job4j.oop;
 
 public class JSONRepor extends TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return "{" + System.lineSeparator()
                 + name + " : " + "name" + System.lineSeparator()
