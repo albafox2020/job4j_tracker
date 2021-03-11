0502ffba1dd94c1df7e3b8b5c66ae9ffe55f39b8
Albina <albafox@yandex.ru>
2021-03-11 22:10:34 +0300

    1. Чтение из консоли. Класс Scanner. [#472798]

---
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        Scanner sc = new Scanner(System.in);
+        String answ = sc.nextLine();
+        System.out.println("Answer: " + answ);
+        int answer = new Random().nextInt(3);
+        switch (answer) {
+            case (1):
+                System.out.println("Да");
+                break;
+            case (2):
+                System.out.println("Нет");
+                break;
+            case (3):
+                System.out.println("Может быть");
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + answer);
+        }
+    }
+}
