65219ff44567ea3dc8532cd7c449f4170d43df60
Albina <albafox@yandex.ru>
2021-03-15 21:15:20 +0300

    2.1. Реализация класса StartUI [#472810]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -57,7 +57,7 @@ public class StartUI {
                 Item[] item = tracker.findByName(name);
                 if (item.length > 0) {
                     for (int i = 0; i < item.length; i++) {
-                        System.out.println(item);
+                        System.out.println(item[i]);
                     }
                 } else {
                     System.out.println("Application with this name was not found" + name);
