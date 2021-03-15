c6c78758d0cca7cdc2aba64a8b25c8734c6963e9
Albina <albafox@yandex.ru>
2021-03-15 22:25:12 +0300

    4.2. Статические методы. [#472805]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,58 +1,81 @@
 package ru.job4j.tracker;
 
 public class StartUI {
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Create a new Item ====");
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+
+    public static void showAllItems(Tracker tracker) {
+        Item[] temp = tracker.findAll();
+        for (Item item : temp) {
+            System.out.println(item);
+        }
+    }
+
+    public static void replaceItemItem(Input input, Tracker tracker) {
+        System.out.println("=== Edit item ====");
+        int id = Integer.parseInt(input.askStr("Enter id for correction: "));
+        String name = input.askStr("Enter name item: ");
+        Item items = new Item(name);
+        if (tracker.replace(id, items)) {
+            System.out.println("Successfully");
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void deteleItem(Input input, Tracker tracker) {
+        int id = Integer.parseInt(input.askStr("Enter id for removing: "));
+        if (tracker.delete(id)) {
+            System.out.println("Successfully");
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void findById(Input input, Tracker tracker) {
+        int id = Integer.parseInt(input.askStr("Enter id for search: "));
+        Item news = new Item(input.askStr(" "));
+        Item temp = tracker.findById(id);
+        if (temp != null) {
+            System.out.println(temp);
+        } else {
+            System.out.println("Application with this id was not found" + id);
+        }
+    }
+
+    public static void findByName(Input input, Tracker tracker) {
+        int id = Integer.parseInt(input.askStr("Enter id for search: "));
+        Item news = new Item(input.askStr(" "));
+        Item temp = tracker.findById(id);
+        if (temp != null) {
+            System.out.println(temp);
+        } else {
+            System.out.println("Application with this id was not found" + id);
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
             int select = Integer.valueOf(input.askStr("Select: "));
             if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                String name = input.askStr("Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
-                System.out.print("Application added");
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                Item[] temp = tracker.findAll();
-                for (Item item : temp) {
-                    System.out.println(item);
-                }
+                StartUI.showAllItems(tracker);
             } else if (select == 2) {
-                System.out.println("=== Edit item ====");
-                int id = Integer.parseInt(input.askStr("Enter id for correction: "));
-                String name = input.askStr("Enter name item: ");
-                Item items = new Item(name);
-                if (tracker.replace(id, items)) {
-                    System.out.println("Successfully");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.replaceItemItem(input, tracker);
             } else if (select == 3) {
-                int id = Integer.parseInt(input.askStr("Enter id for removing: "));
-                if (tracker.delete(id)) {
-                    System.out.println("Successfully");
-                } else {
-                    System.out.println("Error");
-                }
+                StartUI.deteleItem(input, tracker);
             } else if (select == 4) {
-                int id = Integer.parseInt(input.askStr("Enter id for search: "));
-                Item news = new Item(input.askStr(" "));
-                Item temp = tracker.findById(id);
-                if (temp != null) {
-                    System.out.println(temp);
-                } else {
-                    System.out.println("Application with this id was not found" + id);
-                }
+                StartUI.findById(input, tracker);
             } else if (select == 5) {
-                String name = input.askStr("Enter name for search: ");
-                Item[] item = tracker.findByName(name);
-                if (item.length > 0) {
-                    for (int i = 0; i < item.length; i++) {
-                        System.out.println(item[i]);
-                    }
-                } else {
-                    System.out.println("Application with this name was not found" + name);
-                }
+                StartUI.findByName(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
