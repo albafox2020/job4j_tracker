7e92658652573ee8753d39e6e4a1299f423cf018
Albina <albafox@yandex.ru>
2021-04-03 00:13:49 +0300

    2. Комбинированный компаратор [#472774]

--- src/main/java/ru/job4j/tracker/ShowAllItemsWaning.java
+++ src/main/java/ru/job4j/tracker/ShowAllItemsWaning.java
@@ -1,23 +1,23 @@
 package ru.job4j.tracker;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class ShowAction implements UserAction {
+public class ShowAllItemsWaning implements UserAction {
     private final Output out;
 
-    public ShowAction(Output out) {
+    public ShowAllItemsWaning(Output out) {
         this.out = out;
     }
 
     @Override
     public String name() {
-        return "Show all items";
+        return "Show all items by waning";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
         List<Item> temp = tracker.findAll();
+        temp.sort(new SortByNameWaning());
         for (Item item : temp) {
             out.println(item);
         }
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.collection;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndProrityDoubleName() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(1));
+    }
+
+    @Test
+    public void whenIncrByName() {
+        Comparator<Job> cmpName = new JobIncrByName();
+        int rsl = cmpName.compare(
+                new Job("Alex", 3),
+                new Job("Boris", 2)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenIncrDoubleName() {
+        Comparator<Job> cmpName = new JobIncrByName();
+        int rsl = cmpName.compare(
+                new Job("Alex", 3),
+                new Job("Alex", 2)
+        );
+        assertThat(rsl, lessThan(1));
+    }
+
+    @Test
+    public void whenDestByName() {
+        Comparator<Job> cmpName = new JobDescByName();
+        int rsl = cmpName.compare(
+                new Job("Konstantin", 1),
+                new Job("Boris", 2)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenIncrByPriority() {
+        Comparator<Job> cmpName = new JobIncrByPriority();
+        int rsl = cmpName.compare(
+                new Job("Konstantin", 1),
+                new Job("Boris", 2)
+        );
+        assertThat(rsl, lessThan(1));
+    }
+
+    @Test
+    public void whenDescByPriority() {
+        Comparator<Job> cmpName = new JobDescByName();
+        int rsl = cmpName.compare(
+                new Job("Konstantin", 1),
+                new Job("Boris", 2)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+}
