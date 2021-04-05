cd27ba5f65a92f9afdf86996cedecb9e54b70fd7
Albina <albafox@yandex.ru>
2021-04-05 21:26:40 +0300

    0. Сортировка [#472775]

--- src/test/java/ru/job4j/sort/SortItemWTest.java
+++ src/test/java/ru/job4j/sort/SortItemWTest.java
@@ -9,10 +9,10 @@ import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class SortItemTest {
+public class SortItemWTest {
 
     @Test
-    public void whenHaveListThenSortedAtLengthName() {
+    public void whenHaveListThenSortedAtLengthNameWaning() {
         Item itemthree = new Item("AAA");
         Item itemone = new Item("A");
         Item itemtwo = new Item("AA");
@@ -20,8 +20,8 @@ public class SortItemTest {
         items.add(itemone);
         items.add(itemtwo);
         items.add(itemthree);
-        SortItem sort = new SortItem();
-        List<Item> result = sort.sortByName(items);
-        assertThat(result.get(1), is(itemtwo));
+        SortItemW sortW = new SortItemW();
+        List<Item> result = sortW.sortByNameWaning(items);
+        assertThat(result.get(0), is(itemthree));
     }
 }
