cbc13a78f0df17930366e16c58c08ebb0f71e127
Albina <albafox@yandex.ru>
2021-04-12 21:35:47 +0300

    2.2. Лямбда блок [#472602]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,17 @@
+package ru.job4j.lambda;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> compText = (left, right) -> {
+            System.out.println("compare - " + right + " : " + left);
+            return right.compareTo(left);
+        };
+
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+    }
+}
