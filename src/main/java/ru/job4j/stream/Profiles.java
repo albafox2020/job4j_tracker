4266d8e253ce9b74be1e03ef5589f53c376991a8
Albina <albafox@yandex.ru>
2021-04-16 23:49:12 +0300

    2. Список адресов [#472611]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    static List<Address> collect(List<Profile> profiles) {
+    public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
